a6f415ae128a9013b4d31126491a57d7afbecacf
HubertFus <hubertfusiarz@gmail.com>
2024-03-04 10:31:03 +0100

    feat:dm to traget user

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ load_dotenv(dotenv_path)
 
 token = os.environ.get("TOKEN")
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-
+targets = os.environ.get("TARGETS").split(";")
 
 @bot.event
 async def on_ready():
     print(f'Zalogowano jako {bot.user.name} ({bot.user.id})')
     for guild in bot.guilds:
         for member in guild.members:
-            print(member.activity)
+            for target in targets:
+                if target == f'{member.id}':
+                    dm_channel = await member.create_dm()
+                    await dm_channel.send("test")
+
+
+
 
 @bot.event
 async def on_message(message):
